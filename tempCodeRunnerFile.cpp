4279aafc959e9737203b7406938d54347d1c99d9
Lê Thành Đạt <91755733+lds217@users.noreply.github.com>
2024-09-16 15:41:02 -0400

    clean

--- tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-NeuralNetwork net(X_train, Y_train, 0.001, 1);
-    net.train();
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    double a = 0.014;
-    cout<<a;
-    return 0;
-}
